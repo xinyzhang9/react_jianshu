e9b728ae6be85cdf87cdc5ccee213bec573756ab
Xinyang <xinyzhang9@gmail.com>
2017-06-16 02:36:46 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './helloWorld';
+
+ReactDOM.render(
+  <HelloWorld name1='Tom' name2='Jason'/>,
+  document.getElementById('root')
+);
